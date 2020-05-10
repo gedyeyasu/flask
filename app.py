00b77d55edce0ed9af9e03b4026bd08c5d47d2d8
gedyeyasu <gedyeyasu@gmail.com>
2020-05-10 23:55:06 +0300

    created rendering template

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Hello flask"
+	headline="method to import"
+	return render_template("index.html", headline=headline)
 
 @app.route("/gedy")
 def gedy():
 	return "Hello admin"
+
+@app.route("/<string:name>")
+def hello(name):
+	name=name.capitalize()
+	return f"Welcome, {name}!"
 
