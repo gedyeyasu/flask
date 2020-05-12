080b5d42c1a35daebf650897e5a997c7b6c90a6e
gedyeyasu <gedyeyasu@gmail.com>
2020-05-12 11:53:47 +0300

    got taste of flask

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/isitnewyear")
 def index():
-	headline="method to import"
-	return render_template("index.html", headline=headline)
+	now=datetime.datetime.now()
+	newyear= now.month==1 and now.day==1
+	
+	return render_template("index.html", headline=newyear, date=now)
+
+@app.route("/list")
+def list():
+	names=["Gedeon", "Adnrew", "Musk"]
+	return render_template("index.html", name=names)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+	name=request.form.get("name")
+	message=request.form.get("message")
+
+	return render_template("hello.html", name=name, message=message)
+@app.route("/more")
+def more():
+	return render_template("more.html")
 
 @app.route("/gedy")
 def gedy():
@@ -16,3 +34,7 @@ def hello(name):
 	name=name.capitalize()
 	return f"Welcome, {name}!"
 
+@app.route("/bye")
+def bye():
+	headline="Good bye!"
+	return render_template("index.html", headline=headline)
